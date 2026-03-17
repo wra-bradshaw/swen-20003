64b0f240c230eedfe29c6d818e8293c26a783c25
will <will.bradshaw50@gmail.com>
2026-03-17 16:35:47 +1100

    feat: add bageltest

--- projects/lecw3/src/main/java/org/example/lecw3/App.java
+++ projects/lecw3/src/main/java/org/example/lecw3/App.java
@@ -1,10 +1,8 @@
 package org.example.lecw3;
 
-import org.example.utilities.Circle;
-
 public class App {
   public static void main(String[] args) {
-    Circle circle = new Circle(1, 2, 3);
-    System.out.println(circle);
+    BagelTest game = new BagelTest();
+    game.run();
   }
 }
